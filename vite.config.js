ae20ccbac2ebd7f8773a5397ee55dffba82067c4
Кукин Павел Анатольевич <kukin@office.promit-ek.ru>
2026-08-24 14:06:47 +0500

    изменил путь в base

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/',
+  base: '/k-vue-party/',
   server: {
     host: true,
     port: 5173
